c060cd249072e6ba1b97d48cdfaedf36439e06b8
Shyam361 <114291764+Shyam361@users.noreply.github.com>
2024-07-18 04:49:43 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,27 @@
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail=undefined
+const id=Symbol("123")
+const anotherId=Symbol("123")
+
+console.log(id==anotherId)
+console.log(id)
+console.log(anotherId);
+
+const bigNumber=12345678987654n
+console.log(typeof(bigNumber),bigNumber);
+
+const heros=["shaktiman","IronMan","Hulk"]
+
+let myObj={
+    name:"shyam",
+    age:22,
+}
+
+const myfunction=function(){
+    console.log("Hello World!")
+}
+console.log(typeof myfunction)
+myfunction()
