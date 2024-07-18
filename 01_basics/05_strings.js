11240d28e4caf1f56ac87351932a2157f43139c1
Shyam361 <114291764+Shyam361@users.noreply.github.com>
2024-07-18 16:35:16 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,4 +3,11 @@ const repoCount=50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
-const gameName="Game of Thrones";
+const gameName=new String('Game of Thrones')
+
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
